d24fb226b437c60cc0821dfb865f2b7d73a73bd5
dongheok <piouy_@naver.com>
2020-06-28 21:56:35 +0900

    ## 레이아웃 css 버그 수정

--- src/layout/header/styles.jsx
+++ src/layout/header/styles.jsx
@@ -1,30 +1,30 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100%;
-  background: rgba(20, 20, 20, 1);
-  & ul {
-    display: flex;
-    & li {
-      & a {
-        display: block;
-        width: 80px;
-        height: 60px;
-        line-height: 60px;
-        color: #fff;
-        text-align: center;
-        font-size: 14px;
-        border-bottom: 4px solid #000;
-        box-sizing: border-box;
-        transition: all 0.4s ease;
-      }
-      &.On a {
-        border-bottom: 4px solid #27ae60;
-      }
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    background: rgba(20, 20, 20, 1);
+    & ul {
+        display: flex;
+        & li {
+            & a {
+                display: block;
+                width: 80px;
+                height: 60px;
+                line-height: 60px;
+                color: #fff;
+                text-align: center;
+                font-size: 14px;
+                border-bottom: 4px solid #000;
+                box-sizing: border-box;
+                transition: all 0.4s ease;
+            }
+            &.On a {
+                border-bottom: 4px solid #27ae60;
+            }
+        }
     }
-  }
 `;
 export default Wrapper;
