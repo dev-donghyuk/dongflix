8592ca0d0c90a641ee8cbb6ac44ebac8eac80a1f
김동혁 <piouy_@svcorps.dev>
2020-07-18 18:33:39 +0900

    ## push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Routes from "./Routes";
+import "./css/reset.css";
+import "./css/global.css";
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// ie 보기 위함
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
+
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./css/reset.css";
-import "./css/global.css";
+
 import "./api";
 ReactDOM.render(
   <React.StrictMode>
