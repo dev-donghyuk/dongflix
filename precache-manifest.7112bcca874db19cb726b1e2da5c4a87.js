06fd600e3d431e2886266aa28bcaf0b7cec14215
김동혁 <piouy_@svcorps.dev>
2020-07-21 19:26:12 +0900

    Updates

---
+++ precache-manifest.7112bcca874db19cb726b1e2da5c4a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cfcfb87c069cbccdaf5d6fe5a17c85",
+    "url": "/dongflix/index.html"
+  },
+  {
+    "revision": "f2aa0ae178f727a7f47f",
+    "url": "/dongflix/static/css/main.66e4c2fb.chunk.css"
+  },
+  {
+    "revision": "e13c78372244bc55918c",
+    "url": "/dongflix/static/js/2.041c98c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dongflix/static/js/2.041c98c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2aa0ae178f727a7f47f",
+    "url": "/dongflix/static/js/main.8ecc9542.chunk.js"
+  },
+  {
+    "revision": "e14699baf653ed778a2b",
+    "url": "/dongflix/static/js/runtime-main.388dbd0f.js"
+  }
+]);
