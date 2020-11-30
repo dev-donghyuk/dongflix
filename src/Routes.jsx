aea2d24121ce3e307392965f8aadf5cb97b3075d
dev-donghyuk <piouy_@naver.com>
2020-11-30 17:19:04 +0900

    ## Routes.js => Redirect to를 '/'로 변경

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // layout
-import Layout from './layout';
+import Layout from "./layout";
 
 // pages
-import Movie from 'pages/movie';
-import Tv from 'pages/tv';
-import Search from 'pages/search';
-import Detail from 'pages/detail';
+import Movie from "pages/movie";
+import Tv from "pages/tv";
+import Search from "pages/search";
+import Detail from "pages/detail";
 
 const Routes = () => {
-   return (
-      <BrowserRouter>
-         <Layout>
-            <Switch>
-               <Route path="/" exact component={Movie} />
-               <Route path="/movie" exact component={Movie} />
-               <Route path="/tv" exact component={Tv} />
-               <Route path="/search" component={Search} />
-               <Route path="/movie/detail/:id" component={Detail} />
-               <Route path="/tv/detail/:id" component={Detail} />
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Movie} />
+          <Route path="/movie" exact component={Movie} />
+          <Route path="/tv" exact component={Tv} />
+          <Route path="/search" component={Search} />
+          <Route path="/movie/detail/:id" component={Detail} />
+          <Route path="/tv/detail/:id" component={Detail} />
 
-               <Redirect from="*" to="/movie" />
-            </Switch>
-         </Layout>
-      </BrowserRouter>
-   );
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 export default Routes;
