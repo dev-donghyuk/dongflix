26430daaba0d7be467d85e16d5acca3674b835b0
dev-donghyuk <piouy_@naver.com>
2020-11-30 18:00:25 +0900

    ## detail페이지 text 가로값 퍼센트 처리

--- src/pages/detail/styles.tsx
+++ src/pages/detail/styles.tsx
@@ -121,7 +121,7 @@ export const Wrapper = styled.div`
                   }
                }
                & p {
-                  width: 635px;
+                  width: 75%;
                   line-height: 24px;
                   margin-top: 24px;
                   color: rgba(255, 255, 255, 0.8);
