f8a51690244282ac04d05fb8a168a22e07b317fa
dev-donghyuk <piouy_@naver.com>
2020-12-02 14:41:02 +0900

    Updates

---
+++ precache-manifest.733e796ae9585d51a3999dfc7596179e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b271132a10f57c02e5e8c02248c666",
+    "url": "/dongflix/index.html"
+  },
+  {
+    "revision": "2390bceda0b3bc69d0ee",
+    "url": "/dongflix/static/css/main.4303761d.chunk.css"
+  },
+  {
+    "revision": "1fbee611b4f796413d97",
+    "url": "/dongflix/static/js/2.43acfb2c.chunk.js"
+  },
+  {
+    "revision": "2390bceda0b3bc69d0ee",
+    "url": "/dongflix/static/js/main.d6673d52.chunk.js"
+  },
+  {
+    "revision": "254d244015eac44bbc76",
+    "url": "/dongflix/static/js/runtime-main.feeba504.js"
+  }
+]);
